31f79822fef5ecc6189364176ce04145340e14be
ryan <164325424+ryansukangewe@users.noreply.github.com>
2024-09-20 12:11:19 +0700

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,93 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-app.js";
+// import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-auth.js";
+import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-database.js";
+// import { getDatabase } from "firebase/database";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDKPZaAURqisN1_CJaA7uY5whS9FqfKFhU",
+  authDomain: "webcrud-59844.firebaseapp.com",
+  databaseURL: "https://webcrud-59844-default-rtdb.firebaseio.com",
+  projectId: "webcrud-59844",
+  storageBucket: "webcrud-59844.appspot.com",
+  messagingSenderId: "365139551849",
+  appId: "1:365139551849:web:84ee9d5c391095ce89a4f8",
+  measurementId: "G-SN71R8EXBZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const database = getDatabase(app);
+let signinButton = document.getElementById("signin-button");
+let signupButton = document.getElementById("signup-button");
+
+signupButton.addEventListener("click", (e) => {
+  let name = document.getElementById("name").value;
+  let nohp = document.getElementById("nohp").value;
+  let emailSignup = document.getElementById("email_signup").value;
+  let passwordSignup = document.getElementById("psw_signup").value;
+
+  createUserWithEmailAndPassword(auth, emailSignup, passwordSignup)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+
+      set(ref(database, "users/" + user.uid), {
+        name: name,
+        nohp: nohp,
+        email: emailSignup,
+        password: passwordSignup
+      })
+        .then(() => {
+          // Data saved successfully!
+          alert("user telah sukses dibuat");
+        })
+        .catch((error) => {
+          //the write failed
+          alert(error);
+        });
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage);
+    });
+});
+
+signinButton.addEventListener("click", (e) => {
+  let emailSignin = document.getElementById("email_signin").value;
+  let passwordSignin = document.getElementById("psw_signin").value;
+  signInWithEmailAndPassword(auth, emailSignin, passwordSignin)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      let lgDate = new Date();
+      update(ref(database, "users/" + user.uid), {
+        last_login: lgDate
+      })
+        .then(() => {
+          // Data saved successfully!
+          //   alert("user telah sukses login");
+          location.href = "http://127.0.0.1:5500/auth_firebase_cdn/admin.html";
+        })
+        .catch((error) => {
+          //the write failed
+          alert(error);
+        });
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage);
+    });
+  signOut(auth)
+    .then(() => {})
+    .catch((error) => {});
+});
